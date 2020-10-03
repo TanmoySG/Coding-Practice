5b76e2f9056dc0f332059bd5f8939466624f2944
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2020-10-03 21:15:00 +0530

    Update brackets.py

--- brackets.py
+++ brackets.py
@@ -1,1 +1,31 @@
 
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input
+string is valid. An input string is valid if –
+• Open brackets must be closed by the same type of brackets.
+• Open brackets must be closed in the correct order.
+'''
+
+def balanced_brackets(s):
+    s = list(s)
+    brackets = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+    }
+    stack = []
+    for i in s:
+        if not stack:
+            stack.append(i)
+        elif i not in brackets.keys():
+            stack.append(i)
+        elif brackets[i] == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(i)
+        
+    return True if len(stack) == 0 else False
+
+s = input()
+result = balanced_brackets(s)
+print(result)
