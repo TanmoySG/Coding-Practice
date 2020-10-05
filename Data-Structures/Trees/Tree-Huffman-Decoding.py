afb7f97c9f4b582f7c1f834ab190dca2f584b322
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2020-10-05 22:45:32 +0530

    Create Tree-Huffman-Decoding.py

---
+++ Data-Structures/Trees/Tree-Huffman-Decoding.py
@@ -0,0 +1,19 @@
+"""
+class Node:
+    def __init__(self, freq,data):
+        self.freq= freq
+        self.data=data
+        self.left = None
+        self.right = None
+"""        
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def decodeHuff(root, s):
+    #Enter Your Code Here
+    string = []
+    curr = root
+    for i in s:
+        curr = curr.right if i == '1' else curr.left
+        if curr.right is None and curr.left is None:
+            string.append(curr.data)
+            curr = root
