b86caefc26f12a3e0f640180f749b0a7fb03f3bc
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2020-10-05 22:59:29 +0530

    Create Tree-Preorder-Traversal.py

---
+++ Data-Structures/Trees/Tree-Preorder-Traversal.py
@@ -0,0 +1,15 @@
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    if root:
+        print(root.info, end=" ")
+        preOrder(root.left)
+        preOrder(root.right)
+
